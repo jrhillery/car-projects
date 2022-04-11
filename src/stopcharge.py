1cbaba1594947b8c3cf1725e92de44b5d505f779
John Hillery <John@localhost>
2022-04-11 13:47:49 -0400

    Add command line arg to enable charging

--- src/stopcharge.py
+++ src/stopcharge.py
@@ -1,18 +1,37 @@
 
 import json
 import logging
+from argparse import ArgumentParser, Namespace
 from datetime import timedelta
 from logging.config import dictConfig
 from pathlib import Path
 
 import sys
+from math import isqrt
 from requests import request
 from time import time
 
 
+class CarDetails(object):
+
+    def __init__(self, vehicleState: dict, callTime: float):
+        self.vin: str = vehicleState["vin"]
+        self.chargeState: dict = vehicleState["charge_state"]
+        self.displayName: str = vehicleState["display_name"]
+        self.chargingState: str = self.chargeState["charging_state"]
+        self.chargeLimit: int = self.chargeState["charge_limit_soc"]
+        self.limitMinPercent: int = self.chargeState["charge_limit_soc_min"]
+        lastSeen = self.chargeState["timestamp"] * 0.001  # convert ms to seconds
+        self.sinceLastSeen = timedelta(seconds=int(callTime - lastSeen + 0.5))
+    # end __init__(dict, float)
+
+# end class CarDetails
+
+
 class StopCharge(object):
     """Stops vehicles from charging if plugged in"""
-    def __init__(self):
+    def __init__(self, args: Namespace):
+        self.enable: bool = args.enable
         self.headers = {
             "Accept": "application/json",
             "Authorization": f"Bearer {StopCharge.loadToken()}"
@@ -66,7 +85,7 @@ class StopCharge(object):
     def setChargeLimit(self, vin: str, percent: int) -> bool:
         url = f"https://api.tessie.com/{vin}/command/set_charge_limit"
         queryParams = {
-            "retry_duration": 40,
+            "retry_duration": 60,
             "wait_for_completion": "true",
             "percent": percent
         }
@@ -80,40 +99,80 @@ class StopCharge(object):
             return False
     # end setChargeLimit(str, int)
 
-    def stopChargingCar(self, vehicleState: dict, callTime: float):
+    def startCharging(self, vin: str):
+        url = f"https://api.tessie.com/{vin}/command/start_charging"
+        queryParams = {
+            "retry_duration": 60,
+            "wait_for_completion": "true"
+        }
+
+        response = request("GET", url, params=queryParams, headers=self.headers)
+
+        if response.status_code == 200:
+            return True
+        else:
+            logging.error(response.text)
+            return False
+    # end startCharging(str)
+
+    def enableCarCharging(self, dtls: CarDetails):
+        """Raise the charge limit to mean if minimum, then start charging if stopped"""
+
+        if dtls.chargeLimit == dtls.limitMinPercent:
+            meanLimit = isqrt(dtls.limitMinPercent * dtls.chargeState["charge_limit_soc_std"])
+
+            if self.setChargeLimit(dtls.vin, meanLimit):
+                logging.info(f"{dtls.displayName} charge limit changed"
+                             f" from {dtls.chargeLimit}"
+                             f" to {meanLimit}")
+
+        if dtls.chargingState == "Stopped" or dtls.chargingState == "Complete":
+            if self.startCharging(dtls.vin):
+                logging.info(f"{dtls.displayName} charging started")
+    # end enableCarCharging(CarDetails)
+
+    def stopChargingCar(self, dtls: CarDetails):
         """Lower the charge limit to minimum if plugged in and not minimum already"""
-        vin: str = vehicleState["vin"]
-        chargeState: dict = vehicleState["charge_state"]
-        displayName: str = vehicleState["display_name"]
-        chargingState: str = chargeState["charging_state"]
-        chargeLimit: int = chargeState["charge_limit_soc"]
-        limitMinPercent: int = chargeState["charge_limit_soc_min"]
-        lastSeen: float = chargeState["timestamp"] * 0.001  # convert ms to seconds
 
-        # log the current charging state
-        timeAgo = timedelta(seconds=int(callTime - lastSeen + 0.5))
-        logging.info(f"{displayName} is {self.getStatus(vin)}"
-                     f" with charge limit {chargeLimit}"
-                     f"; charging state is {chargingState} {timeAgo} ago")
-
-        if chargingState != "Disconnected" and chargeLimit > limitMinPercent:
+        if dtls.chargingState != "Disconnected" and dtls.chargeLimit > dtls.limitMinPercent:
             # this vehicle is plugged in and not set to charge limit minimum already
 
-            if self.setChargeLimit(vin, limitMinPercent):
-                logging.info(f"{displayName} charge limit changed"
-                             f" from {chargeLimit}"
-                             f" to {limitMinPercent}")
-    # end stopChargingCar(dict, float)
+            if self.setChargeLimit(dtls.vin, dtls.limitMinPercent):
+                logging.info(f"{dtls.displayName} charge limit changed"
+                             f" from {dtls.chargeLimit}"
+                             f" to {dtls.limitMinPercent}")
+    # end stopChargingCar(CarDetails)
 
     def main(self) -> None:
         vehicles = self.getStateOfActiveVehicles()
         callTime = time()
 
         for vehicle in vehicles:
-            self.stopChargingCar(vehicle["last_state"], callTime)
+            carDetails = CarDetails(vehicle["last_state"], callTime)
+
+            # log the current charging state
+            logging.info(f"{carDetails.displayName} is {self.getStatus(carDetails.vin)}"
+                         f" with charge limit {carDetails.chargeLimit}"
+                         f"; charging state is {carDetails.chargingState}"
+                         f" {carDetails.sinceLastSeen} ago")
+
+            if self.enable:
+                self.enableCarCharging(carDetails)
+            else:
+                self.stopChargingCar(carDetails)
     # end main()
 
 # end class StopCharge
+
+
+def parseArgs() -> Namespace:
+    """Parse command line arguments"""
+    ap = ArgumentParser(description="Module to control car charging")
+    ap.add_argument("-e", "--enable", action="store_true",
+                    help="enable charging (default is to disable)")
+
+    return ap.parse_args()
+# end parseArgs()
 
 
 def configLogging() -> None:
@@ -125,9 +184,10 @@ def configLogging() -> None:
 
 
 if __name__ == "__main__":
+    clArgs = parseArgs()
     configLogging()
     try:
-        stopChrg = StopCharge()
+        stopChrg = StopCharge(clArgs)
         stopChrg.main()
     except Exception as xcpt:
         logging.error(xcpt)
