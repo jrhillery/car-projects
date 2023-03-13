d1c27c6d4d95b6cf97d4e969bc17b358d3126908
John Hillery <John@localhost>
2023-03-13 13:38:59 -0400

    Move juicebox interface and control from car-projects to Prog

--- src/juicebox/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Package to facilitate using a programming interface for JuiceBox devices"""
-from .jbdetails import JbDetails
-from .lgdetails import LgDetails
-from .jbinterface import JbInterface
