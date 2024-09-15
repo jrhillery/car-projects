aef44ed8dd45f468502e8d7e32ef64db0fb56c86
John Hillery <John@localhost>
2024-09-15 14:58:50 -0400

    Increase maximum log file size

--- src/util/configure.py
+++ src/util/configure.py
@@ -54,7 +54,7 @@ class Configure(object):
                     "level": "DEBUG",
                     "formatter": "detail",
                     "filename": filePath,
-                    "maxBytes": 50000,
+                    "maxBytes": 200000,
                     "backupCount": 1,
                     "encoding": "utf-8"
                 }
