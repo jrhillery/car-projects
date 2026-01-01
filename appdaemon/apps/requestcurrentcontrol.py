2939a79b7ae3e8d9b66f837e8b972da76b4e3f46
jrhillery <john.hillery2@gmail.com>
2026-01-01 10:11:46 -0500

    Initial AppDaemon script

---
+++ appdaemon/apps/requestcurrentcontrol.py
@@ -0,0 +1,190 @@
+# appdaemon/apps/requestcurrentcontrol.py
+"""Automatically set cars' request currents based on each cars' charging needs."""
+
+from __future__ import annotations
+
+from collections import deque
+from typing import Any, cast, Generator
+
+from appdaemon.events import EventCallback
+from appdaemon.plugins.hass import Hass
+from appdaemon.state import AsyncStateCallback
+
+from tessie import CarDetails
+
+
+class RequestCurrentControl(Hass):
+    """AppDaemon app to automatically set cars' request currents."""
+    messages: deque[str] = deque()
+    vehicleNames: list[str]
+    totalCurrent: int
+
+    # noinspection PyInvalidCast
+    async def initialize(self) -> None:
+        """Called when AppDaemon starts the app."""
+
+        # Get configuration
+        self.vehicleNames = self.args.get("vehicles", [])
+        self.totalCurrent = self.args.get("totalCurrent", 32)
+
+        # Listen for plug-in events
+        await self.listen_state(
+            cast(AsyncStateCallback, self.handleStaleStateChange),
+            [f"binary_sensor.{name.lower()}_charge_cable" for name in self.vehicleNames],
+            old="off", new="on", eventDesc="plugged in")
+        await self.listen_state(
+            cast(AsyncStateCallback, self.handleStateChange),
+            [f"binary_sensor.{name.lower()}_charge_cable" for name in self.vehicleNames],
+            old="on", new="off", eventDesc="unplugged")
+
+        # Listen for charge stopped events
+        await self.listen_state(
+            cast(AsyncStateCallback, self.handleStateChange),
+            [f"switch.{name.lower()}_charge" for name in self.vehicleNames],
+            old="on", new="off", eventDesc="stopped")
+
+        # Listen for a custom event
+        await self.listen_event(
+            cast(EventCallback, self.handleEvent),
+            "SET_REQUEST_CURRENTS")
+        self.log("Ready to adjust cars' request currents.")
+    # end initialize()
+
+    async def handleStateChange(self, entity: str, attribute: str, old: Any, new: Any,
+                                **kwargs: Any) -> None:
+        """Called when a state changes."""
+        self.log("%s %s", await self.friendly_name(entity), kwargs["eventDesc"])
+        await self.setRequestCurrents()
+    # end handleStateChange(str, str, Any, Any, Any)
+
+    # noinspection PyInvalidCast
+    async def handleStaleStateChange(self, entity: str, attribute: str, old: Any, new: Any,
+                                     **kwargs: Any) -> None:
+        """Called when a state changes with stale charge current data."""
+        self.log("%s %s", await self.friendly_name(entity), kwargs["eventDesc"])
+        _, entityName = await self.split_entity(entity)
+        vehicle_name, _ = entityName.split("_", 1)
+        await self.listen_state(
+            cast(AsyncStateCallback, self.handleStateChange),
+            f"number.{vehicle_name}_charge_current",
+            attribute="last_reported", oneshot=True, eventDesc="reported")
+    # end handleStaleStateChange(str, str, Any, Any, Any)
+
+    async def handleEvent(self, event_type: str, data: dict[str, Any], **kwargs: Any) -> None:
+        """Handle custom event."""
+        self.log("Event %s fired", event_type)
+        await self.setRequestCurrents()
+    # end handleEvent(str, dict[str, Any], Any)
+
+    def logMsg(self, message: str) -> None:
+        """Log an info level message and add it to our list.
+
+        :param message: Message to include
+        """
+        self.log(message)
+        self.messages.append(message)
+    # end logMsg(str)
+
+    def generateMsgs(self) -> Generator[str, None, None]:
+        """Generate each included message once."""
+        while self.messages:
+            yield self.messages.popleft()
+    # end generateMsgs()
+
+    def limitRequestCurrents(self, vehicles: list[CarDetails],
+                             desReqCurrents: list[float]) -> list[int]:
+        """Get corresponding request currents valid for each charge adapter.
+
+        :param vehicles: List of cars to have their request currents limited
+        :param desReqCurrents: Corresponding list of desired request currents (amps)
+        :return: Corresponding list of valid request currents
+        """
+        requestCurrents: list[int] = []
+        remainingCurrent = self.totalCurrent
+
+        for i, dtls in enumerate(vehicles):
+            requestCurrent = dtls.limitRequestCurrent(int(desReqCurrents[i] + 0.5))
+            requestCurrents.append(requestCurrent)
+            remainingCurrent -= requestCurrent
+        # end for
+
+        if remainingCurrent < 0 < len(requestCurrents):
+            # we oversubscribed, reduce the largest request current
+            indices: list[int] = list(range(len(requestCurrents)))
+            indices.sort(key=lambda j: requestCurrents[j], reverse=True)
+            requestCurrents[indices[0]] += remainingCurrent
+
+        return requestCurrents
+    # end limitRequestCurrents(list[CarDetails], list[float])
+
+    def calcRequestCurrents(self, vehicles: list[CarDetails]) -> list[int]:
+        """Calculate the current needed for each vehicle.
+
+        :param vehicles: List of cars to have their request currents calculated
+        :return: list of currents needed
+        """
+        energiesNeeded: list[float] = []
+        totalEnergyNeeded = 0.0
+
+        for dtls in vehicles:
+            energyNeeded = dtls.neededKwh()
+
+            if energyNeeded:
+                self.logMsg(dtls.chargingStatusSummary(energyNeeded))
+
+            energiesNeeded.append(energyNeeded)
+            totalEnergyNeeded += energyNeeded
+        # end for
+
+        # Calculate request currents based on energy needs
+        if totalEnergyNeeded:
+            reqCurrents = [
+                self.totalCurrent * (energy / totalEnergyNeeded) for energy in energiesNeeded
+            ]
+        else:
+            reqCurrent = self.totalCurrent / len(vehicles)
+            reqCurrents = [reqCurrent] * len(vehicles)
+
+        return self.limitRequestCurrents(vehicles, reqCurrents)
+    # end calcRequestCurrents(list[CarDetails])
+
+    async def setRequestCurrents(self) -> None:
+        """Automatically set cars' request currents based on each cars' charging needs."""
+
+        # Get fresh details for all vehicles
+        vehicles = [await CarDetails.fromAdapi(self, vehicleName) for vehicleName in self.vehicleNames]
+
+        reqCurrents = self.calcRequestCurrents(vehicles)
+        indices = list(range(len(vehicles)))
+
+        # To decrease first, sort indices ascending by increase in request current
+        indices.sort(key=lambda i: reqCurrents[i] - vehicles[i].chargeCurrentRequest)
+
+        for idx in indices:
+            dtls = vehicles[idx]
+            if dtls.pluggedInAtHome():
+                reqCurrent = reqCurrents[idx]
+                if reqCurrent != dtls.chargeCurrentRequest:
+                    self.logMsg(
+                        f"{dtls.displayName} request current changing from"
+                        f" {dtls.chargeCurrentRequest} to {reqCurrent} A"
+                    )
+                    await self.call_service(
+                        "number/set_value",
+                        entity_id=dtls.chargeCurrentEntityId,
+                        value=reqCurrent,
+                    )
+                else:
+                    self.logMsg(f"{dtls.displayName} request current already set to {reqCurrent} A")
+            else:
+                self.log(dtls.chargingStatusSummary())
+        # end for
+
+        if self.messages:
+            await self.call_service(
+                "persistent_notification/create",
+                title="Set Cars' Request Currents",
+                message="\n".join(self.generateMsgs()),
+            )
+        self.log("Request currents are set.")
+    # end setRequestCurrents()
---
+++ appdaemon/apps/tessie/cardetails.py
@@ -0,0 +1,149 @@
+
+from __future__ import annotations
+
+import dataclasses
+from typing import Any
+
+from appdaemon import ADAPI
+
+
+@dataclasses.dataclass
+class CarDetails:
+    """Details of a vehicle as reported by Tessie"""
+    TESLA_APP_REQ_MIN_AMPS = 5
+
+    displayName: str
+    shiftState: str
+    chargeCurrentEntityId: str
+    chargeCurrentRequest: int
+    requestMaxAmps: int
+    chargeLimit: int
+    chargingState: str
+    battLevel: float
+    status: str
+    savedLocation: str | None
+    battCapacity: float
+
+    @classmethod
+    async def fromAdapi(cls, ad: ADAPI, vehicleName: str) -> CarDetails:
+        """Gets the details of a vehicle.
+
+        :param ad: AppDaemon api instance
+        :param vehicleName: Name of the vehicle
+        :return: CarDetails instance
+        """
+        vehicle_name = vehicleName.lower()
+        shiftStateState: str = await ad.get_state(f"sensor.{vehicle_name}_shift_state")
+        chargeCurrentEntityId = f"number.{vehicle_name}_charge_current"
+        chargeCurrentState: dict[str, Any] = await ad.get_state(chargeCurrentEntityId, "all")
+        chargeLimitState: str = await ad.get_state(f"number.{vehicle_name}_charge_limit")
+        chargingState: str = await ad.get_state(f"sensor.{vehicle_name}_charging")
+        batteryLevelState: str = await ad.get_state(f"sensor.{vehicle_name}_battery_level")
+        statusState: str = await ad.get_state(f"binary_sensor.{vehicle_name}_status")
+        locationState: str = await ad.get_state(f"device_tracker.{vehicle_name}_location")
+
+        return cls(
+            displayName=vehicleName,
+            shiftState=shiftStateState,
+            chargeCurrentEntityId=chargeCurrentEntityId,
+            chargeCurrentRequest=int(float(chargeCurrentState["state"]) + 0.5),
+            requestMaxAmps=chargeCurrentState["attributes"].get("max"),
+            chargeLimit=int(float(chargeLimitState) + 0.5),
+            chargingState=chargingState,
+            battLevel=float(batteryLevelState),
+            status=statusState,
+            savedLocation=locationState,
+            battCapacity=68.3,
+        )
+
+    def pluggedIn(self) -> bool:
+        return self.chargingState != "disconnected"
+    # end pluggedIn()
+
+    def atHome(self) -> bool:
+        return self.savedLocation == "home"
+    # end atHome()
+
+    def pluggedInAtHome(self) -> bool:
+        return self.pluggedIn() and self.atHome()
+    # end pluggedInAtHome()
+
+    def inPark(self) -> bool:
+        return self.shiftState == "p"
+    # end inPark()
+
+    def awake(self) -> bool:
+        return self.status == "on"
+    # end awake()
+
+    def limitRequestCurrent(self, reqCurrent: int) -> int:
+        """Return a request current that does not exceed
+           - the charge adapter's maximum
+           - the minimum supported by Tesla's app
+        :param reqCurrent: Desired request current (amps)
+        :return: Nearest valid request current
+        """
+        if reqCurrent > self.requestMaxAmps:
+            reqCurrent = self.requestMaxAmps
+
+        if reqCurrent < self.TESLA_APP_REQ_MIN_AMPS and self.pluggedInAtHome():
+            reqCurrent = self.TESLA_APP_REQ_MIN_AMPS
+
+        return reqCurrent
+    # end limitRequestCurrent(int)
+
+    def chargeNeeded(self) -> float:
+        """Return the percent increase the battery needs to reach its charge limit.
+
+        :return: The percent increase needed
+        """
+        chargeLimit = self.chargeLimit
+
+        if chargeLimit <= self.battLevel:
+            return 0.0
+        else:
+            return chargeLimit - self.battLevel
+    # end chargeNeeded()
+
+    def neededKwh(self, plugInNeeded = True) -> float:
+        """Return the energy needed to reach the charge limit, in kWh
+           - this estimate is based on the reported battery charge level
+           - depends on having battery capacity
+        :param plugInNeeded: The car needs to be plugged in at home to return non-zero
+        :return: The energy needed
+        """
+        if not plugInNeeded or self.pluggedInAtHome():
+            return self.chargeNeeded() * 0.01 * self.battCapacity
+        else:
+            return 0.0
+    # end neededKwh(bool)
+
+    def chargingStatusSummary(self, khwNeeded: float = 0.0) -> str:
+        """Return a summary charging status suitable for display.
+
+        :param khwNeeded: The kilowatt-hours below limit to include
+        :return: Summary
+        """
+        parts: list[str] = [f"{self.displayName} was "]
+        if self.inPark():
+            parts.append(f"{self.status}line")
+        else:
+            parts.append("driving")
+        if self.savedLocation:
+            parts.append(f"@{self.savedLocation}")
+        parts.append(f" {self.chargingState}")
+        if self.pluggedIn():
+            parts.append(f" {self.chargeCurrentRequest}/{self.requestMaxAmps}A")
+        parts.append(f", limit {self.chargeLimit}%"
+                     f" and battery {self.battLevel}%")
+        if khwNeeded:
+            parts.append(f" ({khwNeeded:.1f} kWh < limit)")
+
+        return "".join(parts)
+    # end chargingStatusSummary(float)
+
+    def __str__(self) -> str:
+        return f"{self.displayName}@{self.battLevel}%"
+    # end __str__()
+
+# end class CarDetails
