cbbba9cadd55d870e110f2718b900fce7bea0c72
jrhillery <john.hillery2@gmail.com>
2026-05-23 09:10:25 -0400

    add script to restore prior capacity

---
+++ appdaemon/apps/restorepriorstate.py
@@ -0,0 +1,18 @@
+
+from appdaemon import Hass
+
+from tessie import CarDetails
+
+
+class RestorePriorState(Hass):
+
+    def initialize(self):
+        vehicleName = self.args["vehicleName"].lower()
+        capacity = str(self.args["capacity"])
+        dtls = CarDetails.fromAdapi(self, vehicleName)
+        oldCapacity = dtls.batteryCapacity
+
+        dtls.batteryCapacitySensor.set_state(
+            capacity, attributes={"battery_level_added": dtls.persistedBatteryLevelAdded})
+        self.log("%s working capacity estimate restored from %.2f to %s kWh",
+                 dtls.displayName, oldCapacity, capacity)
