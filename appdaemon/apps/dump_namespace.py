5bf8c8392b7a4910f46e839ade1d8f7f7c6394cd
jrhillery <john.hillery2@gmail.com>
2026-06-02 09:12:09 -0400

    allow dumping a list of persistent namespaces

--- appdaemon/apps/dump_namespace.py
+++ appdaemon/apps/dump_namespace.py
@@ -4,10 +4,12 @@ from appdaemon import Hass
 class DumpNamespace(Hass):
 
     def initialize(self):
-        namespace = self.args.get("namespace", "default")
-        self.log(f"=== Namespace dump: {namespace} ===")
-        data = self.get_state(namespace=namespace)
-        assert isinstance(data, dict)
-        for k, v in data.items():
-            self.log(f"  {k}: {v}")
+        for namespace in self.args.get("namespaces", ["default"]):
+            self.log(f"=== Namespace dump: {namespace} ===")
+            if not self.namespace_exists(namespace):
+                self.add_namespace(namespace)
+            data = self.get_state(namespace=namespace)
+            assert isinstance(data, dict)
+            for k, v in data.items():
+                self.log(f"  {k}: {v}")
         self.log("=== End dump ===")
